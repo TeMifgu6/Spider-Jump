5012b64bda9d114c7fe6dcb4db9032e2bfa6e733
TeMifgu6 <112153107@mail.fgu.edu.tw>
2025-03-20 23:29:21 +0800

    332
    嘗試中

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour{
 
     public float speed = 0.1f;
     public float jump = 0.1f;
-    public Rigidbody2D rigid2D;
+    Rigidbody2D rigid2D;
     void Start(){
         print("Start");
+        rigid2D = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
 
     void Update() {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigid2D.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            rigid2D.AddForce(new Vector2(0, jump), ForceMode2D.Force);
             print("Jump");
         }
         if (Input.GetKey(KeyCode.RightArrow))
